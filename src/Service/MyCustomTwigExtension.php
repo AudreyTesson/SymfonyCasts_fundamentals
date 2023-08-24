d4e9aebac98485d13aec25fd65d8c1d63e211534
Audrey Tesson <audrey.tesson.pro@outlook.fr>
2023-08-24 11:07:01 +0200

    done

---
+++ src/Service/MyCustomTwigExtension.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace App\Service;
+
+use Twig\Extension\AbstractExtension;
+
+class MyCustomTwigExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        
+    }
+}
